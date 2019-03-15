03424b915cf691c09732b07fbac17b9a9e1bf020
NoahRottman <40177131+NoahRottman@users.noreply.github.com>
2019-03-15 15:37:15 -0400

    temporarily messy code

--- kmeans3D.py
+++ kmeans3D.py
@@ -67,6 +67,32 @@ def reposition_centroids(data_set, centroids):
 
     return centroids
 
+def reposition_centroids2(data_set, centroids):
+    dist = np.zeros(len(centroids))
+    q_vals = dist
+    prev_dist = dist
+
+    for t in range(len(prev_dist)):
+        prev_dist[t] = 9999
+        q_vals[t] = 9999
+
+    for x in np.arange(-10, 10, 0.1):
+        for y in np.arange(-10, 10, 0.1):
+            for z in np.arange(-10, 10, 0.1):
+                q = [x,y,z]
+                dist[dist!=0] = 0
+                for i in range(len(data_set)):
+                    for j in range(len(centroids)):
+                        if (data_set[i][3] == j):
+                            dist[j] += distance(data_set[i], q)
+                for l in range(len(centroids)):
+                    if(dist[l]<prev_dist[l]): #prev dist is a list for some reason rn
+                        prev_dist[l] = dist[l]
+                        q_vals[l] = q
+
+
+
+    return q_vals
 
 def kmeans(data_set, k, max_iterations):
     centroids = rand_centroids(data_set, k)
@@ -85,6 +111,29 @@ def kmeans(data_set, k, max_iterations):
     return centroids
 
 
+def function_analysis(data_point,range):
+    return (data_point[2])*(range**2) + (data_point[1])*range + data_point[0]
+
+
+def create_Q(n):
+    Q = np.zeros(n,n)
+    for i in range(n):
+        for j in range(n):
+            Q[i][j] = (t**(i+j+1))/(i+j+1)
+
+
+def avg_distortion(data_set,centroids):
+    centroid_counter = np.zeros(len(centroids), 1)
+    distortion = 0
+    for i in range(len(data_set)):
+        for j in range(len(centroids)):
+            if data_set[i][3]==j:
+                centroid_counter[j]+=1
+
+    for k in range(len(data_set)):
+            distortion += (1/centroid_counter[data_set[k][3]])*
+
+
 data_set = np.zeros((1000, 4))
 
 for i in range(len(data_set)):
@@ -93,19 +142,33 @@ for i in range(len(data_set)):
     data_set[i][2] = int(np.random.uniform(-100, 100))
 
 centroids = kmeans(data_set, 8, 100)
-
+rangex = np.linspace(0,1,100)
 
 fig = plt.figure()
-ax = plt.axes(projection='3d')
+#ax = plt.axes(projection='3d')
 colors = ['red', 'blue', 'yellow', 'orange', 'cyan', 'magenta', 'green', 'purple']
 
-for j in range(len(data_set)):
-    ax.scatter3D(data_set[j][0],data_set[j][1], data_set[j][2], marker = 'o', color = colors[int(data_set[j][3])])
+lets_get_messy = []
+centroid_combo = []
 
-for i in range(len(centroids)):
-    ax.scatter3D(centroids[i][0],centroids[i][1], centroids[i][2], marker = 'o', color = 'black', s = 50)
+# for k in range(len(data_set)):
+#     lets_get_messy.append(function_analysis(data_set[k], rangex))
+#     plt.plot(rangex,lets_get_messy[k],color = colors[int(data_set[k][3])])
 
+for l in range(len(centroids)):
+    centroid_combo.append((function_analysis(centroids[l], rangex)))
+    plt.plot(rangex, centroid_combo[l], color=colors[l])
 
+#
+# for j in range(len(data_set)):
+#     ax.scatter3D(data_set[j][0],data_set[j][1], data_set[j][2], marker = 'o', color = colors[int(data_set[j][3])])
+#
+# for i in range(len(centroids)):
+#     ax.scatter3D(centroids[i][0],centroids[i][1], centroids[i][2], marker = 'o', color = 'black', s = 50)
+
+#print(data_set)
+
+#plt.plot(rangex,lets_get_messy)
 plt.show()
 
 # UNCOMMENT THE CODE BELOW TO SEE THE COORDINATES OF THE CENTROIDS
@@ -120,3 +183,6 @@ plt.show()
 #             centroid_counter[j] += 1
 
 # print(centroid_counter)
+
+
+
